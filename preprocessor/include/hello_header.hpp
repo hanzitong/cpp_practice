f6579d0cee16eb5f659f598b34338cc1c34e07e9
Han Zitong <han.zitong@rm.is.tohoku.ac.jp>
2023-10-21 19:33:49 +0900

    del unnecessary files, add some comments for preprocessor derective

--- preprocessor/include/hello_header.hpp
+++ preprocessor/include/hello_header.hpp
@@ -1,11 +1,22 @@
+
+/* these set of macro definiciton are "include guard" */
+// #ifndef MACRO
+// #define MACRO
+// some codes 
+// #endif
+
+
 #ifndef PREPROCESSOR
-#define PREPROCESSOR
+// check if PREPROCESSOR macro is defined.
+// if already defined, skip to #endif so it can prevent include twice.
+// if not defined, execute below codes.
+    #define PREPROCESSOR
+    // define a empty PREPROCESSOR macro here for preventing further second include.
 
-namespace preprocessor{
-
-void hello_func(void);
-
-}   // namespace preprocessor
+    namespace preprocessor{
+    void hello_func(void);
+    }   // namespace preprocessor
 
 #endif
+// end of #ifndef clause
 
