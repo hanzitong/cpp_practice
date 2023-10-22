4d38050887dfd574f5ed1863c68acc2b064ee291
Han Zitong <han.zitong@rm.is.tohoku.ac.jp>
2023-10-23 01:08:51 +0900

    add comment

--- memory/dynamically_vs_statically_secure.cpp
+++ memory/dynamically_vs_statically_secure.cpp
@@ -2,7 +2,9 @@
 /*
 stack memory : it requires variable size when compile.
 heap memory : it does't require variable size when compile.
-this is why if you want secure memory dynamically, you need to secure it in heap.
+this is why if you want to secure memory dynamically, you need to secure it in heap.
+
+eg. std::vector secure memory dynamically so it exists in heap.
 */
 
 
@@ -16,20 +18,9 @@ int main(int argc, char* argv[]){
         return -1;
     }
 
-    // statically secured meory
-    int n_static = LENGTH;
-    double array_static[n_static];  // secure memory statically in stack
-    // array_staitc size is already defined when compiling
-    // (stack memory cannot be secured when run-time)
-    for(int i=0; i<n_static; ++i){
-        *(array_static + i) = 0.;
-        std::cout << *(array_static + i) << " ";
-    }
-    std::cout << std::endl;
-
-
     // dynamically secured memory
-    int n_dynamic = std::atoi(argv[1]);
+    // int n_dynamic = std::atoi(argv[1]);
+    int n_dynamic = argc;
     double *array_dynamic = new double[n_dynamic];  // secure memory dynamically in heap
     // array_dynamic size is not defined when compiling
     // so it needs to dynamically secure in heap meory.
@@ -39,6 +30,20 @@ int main(int argc, char* argv[]){
     }
     std::cout << std::endl;
     delete[] array_dynamic;
+
+
+    // statically secured meory
+    int n_static = LENGTH;
+    // int n_static = argc;    // error if using old compiler.
+    double array_static[n_static];  // secure memory statically in stack
+    // array_staitc size is already defined when compiling
+    // (stack memory cannot be secured when run-time)
+    for(int i=0; i<n_static; ++i){
+        *(array_static + i) = 0.;
+        std::cout << *(array_static + i) << " ";
+    }
+    std::cout << std::endl;
+
 
 
     return 0;
