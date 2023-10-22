e0cab7f904354991884784208de7bffd3ed11917
Han Zitong <han.zitong@rm.is.tohoku.ac.jp>
2023-10-23 00:13:58 +0900

    add comments and refactor in dynamically_vs_statically_secure.cpp

--- memory/dynamically_vs_statically_secure.cpp
+++ memory/dynamically_vs_statically_secure.cpp
@@ -1,4 +1,11 @@
 
+/*
+stack memory : it requires variable size when compile.
+heap memory : it does't require variable size when compile.
+this is why if you want secure memory dynamically, you need to secure it in heap.
+*/
+
+
 #include <iostream>
 #include <cstdlib>
 #define LENGTH 10
@@ -9,27 +16,30 @@ int main(int argc, char* argv[]){
         return -1;
     }
 
-    int n_static = LENGTH; // for statically secured memory
-    int n_dynamic = std::atoi(argv[1]);   // for dynamically secured memory
-
-    double array_static[n_static];    // error or run incorrectly if used for dynamic secure process
-    double *array_dynamic = new double[n_dynamic];
-
-    // // statically secured meory
+    // statically secured meory
+    int n_static = LENGTH;
+    double array_static[n_static];  // secure memory statically in stack
+    // array_staitc size is already defined when compiling
+    // (stack memory cannot be secured when run-time)
     for(int i=0; i<n_static; ++i){
         *(array_static + i) = 0.;
         std::cout << *(array_static + i) << " ";
     }
     std::cout << std::endl;
 
+
     // dynamically secured memory
+    int n_dynamic = std::atoi(argv[1]);
+    double *array_dynamic = new double[n_dynamic];  // secure memory dynamically in heap
+    // array_dynamic size is not defined when compiling
+    // so it needs to dynamically secure in heap meory.
     for(int i=0; i<n_dynamic; ++i){
         *(array_dynamic + i) = 0.;
         std::cout << *(array_dynamic + i) << " ";
     }
     std::cout << std::endl;
-
     delete[] array_dynamic;
+
 
     return 0;
 }
