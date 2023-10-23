7de8d9b1ba5e136c89a661116bf4a764c126555a
Han Zitong <han.zitong@rm.is.tohoku.ac.jp>
2023-10-24 01:16:14 +0900

    finish class_copy_constructor.cpp

--- grammar/class/class_copy_constructor.cpp
+++ grammar/class/class_copy_constructor.cpp
@@ -2,7 +2,10 @@
 /*
 for using copy constractor, we need to be careful about difference of deep copy and sharow copy.
 deep copy: duplicate whole resources which pointer is pointing.
-shalow copy: copy only pointer's address. 
+shalow copy: copy only pointer's address so the pointing resources are same.(it causes many bags) 
+deep copy: it can be conducted with only copy-constractor. it won't copy the member pointer directlly.
+            but we should remember that even copy-constractor is used, it's default is shallow copy.
+            we should 
 
 
 */
@@ -14,56 +17,89 @@ shalow copy: copy only pointer's address.
 
 class Pos
 {
-public:
+// private:     // default
     int x;
     int y;
+    int *arrow; // pointer member !!!!!!!!!!!!!!
+public:
     Pos(int tempx, int tempy);
     Pos(const Pos& pos);
     ~Pos();
+    void get_ab();
+    void set_arrow(int);
+    void set_arrow(int, int);
+    void get_arrow();
 };
 
-
 // normal constractor
-Pos::Pos(int tempx, int tempy){      // constractor
-    std::cout << "called constractor" << std::endl;
+Pos::Pos(int tempx, int tempy){
+    std::cout << "constractor" << std::endl;
     x = tempx;
     y = tempy;
+    arrow = new int(x + y);  // new: it returns pointer whose value is initialized with x+y.
 }
 
 // copy constractor
-// argument needs Pos it's own instance so its copy constructor.
-Pos::Pos(const Pos& pos){        // copy constructor
-    std::cout << "called copy constractor" << std::endl;
+Pos::Pos(const Pos& pos){// argument needs Pos it's own instance so we recognize it's copy constructor.
+    std::cout << "copy constractor" << std::endl;
     x = pos.x;
     y = pos.y;
+    // secure new memory in heap for arrow member pointer.
+    arrow = new int(1000);
 }
 
 // destractor
 Pos::~Pos(void){
-    std::cout << "called destractor" << std::endl;
+    std::cout << "destractor" << std::endl;
+    // delete arrow;
+}
+
+void Pos::set_arrow(int val){
+    *arrow = val;
+}
+
+void Pos::set_arrow(int val, int vaal){
+    *arrow = val;
+}
+
+void Pos::get_ab(){
+    // std::cout << "a: " << Pos.x << "b: " << Pos.y << std::endl;
+    std::cout << "a: " << x << ",  b: " << y << ", arrow" << *arrow << std::endl;
 }
 
 
-int main(){
-    int a = 5;
 
-    std::cout << "make instance obj1" << std::endl;
+int main(void){
     Pos obj1(1, 2);
-    std::cout << "check obj1's variables x:" << obj1.x << ", y:" << obj1.y << std::endl;
+    Pos obj2(2, 3);
 
-    std::cout << "make obj2 by copying from obj1 with copy constructor" << std::endl;
-    Pos obj2 = obj1;
-    std::cout << "check obj2's variables x:" << obj2.x << ", y:" << obj2.y << std::endl;
+    // watch initial value
+    obj1.get_ab();
+    obj2.get_ab();
 
-    std::cout << "copy obj2 to obj2 by using copy operation" << std::endl;
-    obj1 = obj2;
-    std::cout << "check obj1's variables x:" << obj1.x << ", y:" << obj1.y << std::endl;
+    // shallow copy
+    std::cout << "shallow copy" << std::endl;    
+    obj2 = obj1;    // shallow copy (member pointer will point same resources)
+                    // very easy to produce bags. 
+    obj1.get_ab();
+    obj2.get_ab();
+    // even I changed only obj2, obj1 is also changed. (because of shallow copy, arrow points same memory)
+    std::cout << "set value via arrow" << std::endl;
+    obj2.set_arrow(20);
+    obj1.get_ab();
+    obj2.get_ab();
 
-    std::cout << "finish main loop" << std::endl;
+    // deep copy
+    std::cout << "deep copy" << std::endl;
+    Pos obj3 = obj2;  // use copy constractor (deep copy)
+                            // it can be both shallow-copy(default) and deep-copy.
+                            // if copy constractor specify proceedure, it can be deep copy.
+    std::cout << "set value via arrow" << std::endl;
+    obj2.get_ab();
+    obj3.get_ab();      // obj3.arrow is initialized by copy constructor.
 
 
     return 0;
 }
-
 
 
