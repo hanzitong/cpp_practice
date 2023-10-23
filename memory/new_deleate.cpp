7199e26466f42fad43dc4e14dcb851f75180935e
Han Zitong <han.zitong@rm.is.tohoku.ac.jp>
2023-10-23 21:53:29 +0900

    update new_delete.cpp

--- memory/new_deleate.cpp
+++ memory/new_deleate.cpp
@@ -2,34 +2,51 @@
 
 /*
 stack memory and heap memory exist.
-in C, malloc() allocate memory space on heap memory and free() free allocated memory space.
+in C, calloc(), malloc(), etc, allocate memory space on heap memory and free() free allocated memory space.
 in C++, new operator allocate space on heap memory and deleate operator free it.
 */
 
 #include <iostream>
 
 
-// raw_ptr
 int main(){
+    // declare using new & initialize
     int* ptr = new int(10);
-    std::cout << "ptr = " << *ptr << std::endl;
+
+    // declare using new & assign value
+    int* ptr2 = new int;
+    *ptr2 = 20;
+
+    // watch value
     for(int i=0; i<10; i++){
         *ptr += 1;
+        std::cout << *ptr << " ";
     }
-    std::cout << "ptr = " << *ptr << std::endl;
-    delete ptr; // if forget this sentence, memory-leak occurs.
+    std::cout << std::endl;
 
+    // free secured memory
+    delete ptr; // if forget this sentence, memory-leak occurs.
+    delete ptr2;
+
+    // ===============================
+
+    // declare array memory
     int* array_ptr = new int[5];
+
+    // assign value 
     for(int i=0; i<5; i++){
         array_ptr[i] = i;
     }
 
+    // watch value
     for(int i=0; i<5; i++){
         std::cout << array_ptr[i] << " ";
     }
     std::cout << std::endl;
 
+    // free array memory
     delete[] array_ptr;
+
 
     return 0;
 }
