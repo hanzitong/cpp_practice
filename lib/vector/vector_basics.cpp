22e2082678210429f0162fce823a58d5beaeadd9
Han Zitong <han.zitong@rm.is.tohoku.ac.jp>
2023-12-28 19:48:57 +0900

    add some files

--- lib/vector/vector_basics.cpp
+++ lib/vector/vector_basics.cpp
@@ -6,8 +6,12 @@
 int main (int argc, char* argv[]) {
     std::vector<int> vec (5, 0);    // initialize with 5 elements & 0 value.
 
-    for (int i=0; i<vec.size(); ++i) {
-        std::cout << vec[i] << std::endl;
+    // for (int i=0; i<vec.size(); ++i) {
+    //     std::cout << vec[i] << std::endl;
+    // }
+
+    for (std::vector<int>::iterator it=vec.begin(); it != vec.end(); ++it) {
+        std::cout << *it << std::endl;
     }
 
 
