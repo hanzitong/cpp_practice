7784a42657cf34ac3a096f5cbbb146dc820da9dd
Han Zitong <han.zitong@rm.is.tohoku.ac.jp>
2024-01-07 06:24:05 +0900

    resize.cpp: add str.capacity()

--- lib/string/resize.cpp
+++ lib/string/resize.cpp
@@ -9,12 +9,15 @@ main(){
     std::cin >> str;
 
     std::cout << str.size() << std::endl;   // std::string.size() returns the number of size
+    std::cout << str.length() << std::endl;   // same with std::string.size()
+    std::cout << str.capacity() << std::endl;   // return allocated storage
 
     std::cout << str << std::endl;
     str.resize(5);
     std::cout << str << std::endl;
 
-    std::cout << str.length() << std::endl;   // same with std::string.size()
+    std::cout << str.size() << std::endl;   // std::string.size() returns the number of size
+    std::cout << str.capacity() << std::endl;   // return allocated storage
 
     str.resize(10, '+');    // add '+' to all vacant spaces
     std::cout << str << std::endl;
