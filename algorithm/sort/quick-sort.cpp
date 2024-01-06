f1b77845dadd633359faeed72b3bd8b657134994
Han Zitong <han.zitong@rm.is.tohoku.ac.jp>
2024-01-07 06:12:51 +0900

    add new file quick-sort.cpp which is not implemented

---
+++ algorithm/sort/quick-sort.cpp
@@ -0,0 +1,28 @@
+
+#include <iostream>
+
+#include <algorithm>
+#include <vector>
+
+
+
+int main(){
+    std::vector<int> vec = {1, 2, 3, 4, 5};
+    int N;
+
+    std::cin >> N;
+
+
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
