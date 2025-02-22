8fdbf1e4034b92e21ebf0b6c33dcdde688de8801
Han Zitong <han.zitong@rm.is.tohoku.ac.jp>
2025-02-23 05:41:17 +0900

    add a practice file that overloads operator() in a class

---
+++ overload/operator.cpp
@@ -0,0 +1,51 @@
+
+#include <iostream>
+#include <vector>
+
+class Mul {
+    private:
+        int i_;
+    public:
+        Mul(int input): i_(input) // initializer list
+        {
+            std::cout << "constructor 0 was called" << std::endl;
+        }
+
+        Mul(int input1, int input2): i_(input1 * input2) // overloaded constructor
+        {
+            std::cout << "constructor 1 was called" << std::endl;
+        }
+
+        void i_printer(void) {
+            std::cout << i_ << std::endl;
+        }
+
+        int operator() (int j) {
+            return i_ * j;
+        }
+
+
+};
+
+
+int main() {
+    int i;
+
+    Mul guest1(2);
+    Mul guest2(5);
+    Mul guest3(5, 10);
+
+    guest1.i_printer();
+    guest2.i_printer();
+    guest3.i_printer();
+
+    std::cout << guest1(2) << std::endl;
+    std::cout << guest2(2) << std::endl;
+    std::cout << guest3(2) << std::endl;
+
+
+
+    return 0;
+}
+
+
